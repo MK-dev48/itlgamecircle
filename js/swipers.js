76da7d1715455cdaa17429eb428a8fd75ed16b17
MK-dev48 <mai.koga21@gmail.com>
2023-07-02 23:35:55 +0900

    学祭でるよ

--- js/swipers.js
+++ js/swipers.js
@@ -1,6 +1,7 @@
  'use strict';
 
- var img = new Array(
+ var img = new Array( //順番はこの順番
+                      "images/slide4.png",
                       "images/slide1.png",
                       "images/slide2.png",
                       "images/slide3.png",
@@ -14,16 +15,18 @@
    count++;
    if(count == img.length) count = 0;
 
-
    element.src = img[count];
 
-
-   if(count == 0){
+   if(img[count] == "images/slide1.png"){
    links.href = "infomations.html";
- } else if (count == 1) {
+ } else if (img[count] == "images/slide2.png") {
    links.href = "contents.html";
- }else if (count == 2) {
+ }else if (img[count] == "images/slide3.png") {
   links.href = "contact.html";
+ }else if (img[count] == "images/slide4.png") {
+  links.href = "itlfest2023.html";
  }
+
  setTimeout("imgTimer()",7000);
- }
+ 
+}
